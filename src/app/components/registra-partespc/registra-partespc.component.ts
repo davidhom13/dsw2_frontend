733311f13929e6c6668e889a29d77231bf297c5a
davidhom13 <davidhugo.ortiz@gmail.com>
2022-06-29 12:03:03 -0500

    Creacion de html, css y ts v2

---
+++ src/app/components/registra-partespc/registra-partespc.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-registra-partespc',
+    templateUrl: './registra-partespc.component.html',
+    styleUrls: ['./registra-partespc.component.css']
+  })
+
+  export class RegistraPartesPc implements OnInit{
+    
+    ngOnInit(): void {
+    }
+
+  }
