2fe59e07833add8f307d1edd654176245003684b
davidhom13 <davidhugo.ortiz@gmail.com>
2022-06-30 19:44:12 -0500

    Modificacion de HTMLs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,19 @@ import { RegistraProductoComponent } from './components/registra-producto/regist
 import { CrudProductoComponent } from './components/crud-producto/crud-producto.component';
 import { ConsultaProductoComponent } from './components/consulta-producto/consulta-producto.component';
 import { RegistraPartesPc } from './components/registra-partespc/registra-partespc.component';
+import { RegistraProgramas } from './components/registra-programas/registra-programas.component';
 
 
 
 const routes: Routes = [
   
   {path:"registraProducto", component:RegistraProductoComponent },
-  {path:"registraPartesPc", component:RegistraPartesPc},
   {path:"crudProducto", component:CrudProductoComponent },
   {path:"consultaProducto", component:ConsultaProductoComponent },
+
+
+  {path:"registraPartesPc", component:RegistraPartesPc},
+  {path:"registraProgramas", component:RegistraProgramas}
 
 ];
 
